dea2b6fbd17af242e3b5aa44bce3ba59fd68ed96
Jie Cai <jiecai@pku.stu.edu.com>
2023-07-11 14:34:50 +0800

    dev 0.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,41 @@ import { Space, Avatar } from 'antd';
 import Projection from './Projection';
 import Calculation from './Calculation'
 import { useState, useCallback } from 'react'
+import Login from './Login';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
   const [data, setData] = useState();
   const [range, setRange] = useState([0, 100]);
   const changeData = useCallback((data) => setData(data), []);
   const changeRange = useCallback((range) => setRange(range), []);
+  const changeIsLogin = useCallback((data) => setIsLogin(data), []);
   return (
     <>
-      <Space>
-        <Avatar src="/CLAPA.png" />
-        <h3>CLAPA-OPTICS Camera software</h3>
-      </Space>
-      <div className='wrapper'>
-        <Config />
-      </div>
-      <div className='wrapper'>
-        <Camera range={range} changeData={changeData} changeRange={changeRange} />
-        <Projection data={data} range={range} changeData={changeData}/>
-        <Calculation />
-      </div>
+      {isLogin === false ?
+        <>
+          <Space>
+            <Avatar src="/CLAPA.png" />
+            <h3>CLAPA-OPTICS Camera software</h3>
+          </Space>
+          <Login setIsLogin={changeIsLogin}/>
+        </>
+      :
+        <>
+          <Space>
+            <Avatar src="/CLAPA.png" />
+            <h3>CLAPA-OPTICS Camera software</h3>
+          </Space>
+          <div className='wrapper'>
+            <Config />
+          </div>
+          <div className='wrapper'>
+            <Camera range={range} changeData={changeData} changeRange={changeRange} />
+            <Projection data={data} range={range} changeData={changeData}/>
+            <Calculation />
+          </div>
+        </>
+      }
     </>
   )
 };
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,93 +1,100 @@
-import Login from 'ant-design-pro/lib/Login';
-import { Alert, Checkbox } from 'antd';
+import { Button, Space, Avatar, Checkbox, Form, Input } from 'antd';
+import { useRef } from 'react';
+function Login(props) {
 
-const { Tab, UserName, Password, Mobile, Captcha, Submit } = Login;
+  const {setIsLogin} = props;
 
-class LoginDemo extends React.Component {
-  state = {
-    notice: '',
-    type: 'tab2',
-    autoLogin: true,
+  const [form] = Form.useForm();
+  const formRef = useRef(null);
+
+  const onFinish = (values) => {
+    if(values["password"] === "1234"){
+      setIsLogin(true);
+    }
+    console.log('Success:', values);
   };
-  onSubmit = (err, values) => {
-    console.log('value collected ->', {
-      ...values,
-      autoLogin: this.state.autoLogin,
-    });
-    if (this.state.type === 'tab1') {
-      this.setState(
-        {
-          notice: '',
-        },
-        () => {
-          if (!err && (values.username !== 'admin' || values.password !== '888888')) {
-            setTimeout(() => {
-              this.setState({
-                notice: 'The combination of username and password is incorrect!',
-              });
-            }, 500);
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form 
+      form={form}
+      ref={formRef}
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+        onChange = {
+          () => {
+            form.setFieldsValue(
+              {
+                password: "1234"
+              }
+              )
           }
         }
-      );
-    }
-  };
-  onTabChange = key => {
-    this.setState({
-      type: key,
-    });
-  };
-  changeAutoLogin = e => {
-    this.setState({
-      autoLogin: e.target.checked,
-    });
-  };
-  render() {
-    return (
-      <div className="login-warp">
-        <Login
-          defaultActiveKey={this.state.type}
-          onTabChange={this.onTabChange}
-          onSubmit={this.onSubmit}
-        >
-          <Tab key="tab1" tab="Account">
-            {this.state.notice && (
-              <Alert
-                style={{ marginBottom: 24 }}
-                message={this.state.notice}
-                type="error"
-                showIcon
-                closable
-              />
-            )}
-            <UserName name="username" />
-            <Password name="password" />
-          </Tab>
-          <Tab key="tab2" tab="Mobile">
-            <Mobile name="mobile" />
-            <Captcha onGetCaptcha={() => console.log('Get captcha!')} name="captcha" />
-          </Tab>
-          <div>
-            <Checkbox checked={this.state.autoLogin} onChange={this.changeAutoLogin}>
-              Keep me logged in
-            </Checkbox>
-            <a style={{ float: 'right' }} href="">
-              Forgot password
-            </a>
-          </div>
-          <Submit>Login</Submit>
-          <div>
-            Other login methods
-            <span className="icon icon-alipay" />
-            <span className="icon icon-taobao" />
-            <span className="icon icon-weibo" />
-            <a style={{ float: 'right' }} href="">
-              Register
-            </a>
-          </div>
-        </Login>
-      </div>
-    );
-  }
-};
+      >
+        <Input />
+      </Form.Item>
 
-export default LoginDemo;
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+export default Login;
